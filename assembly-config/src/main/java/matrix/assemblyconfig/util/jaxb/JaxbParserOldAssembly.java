fc6c91ccd255080b191797b45d750f603fdd9b63
pm-swapnil-dubey <swapnil.dubey@pubmatic.com>
2015-01-16 03:11:41 -0500

    Reading old assembly files

--- assembly-config/src/main/java/matrix/assemblyconfig/util/jaxb/JaxbParserOldAssembly.java
+++ assembly-config/src/main/java/matrix/assemblyconfig/util/jaxb/JaxbParserOldAssembly.java
@@ -3,11 +3,13 @@ package matrix.assemblyconfig.util.jaxb;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
-public class OldJaxbPaarser {
+public class JaxbParserOldAssembly {
 	public static <T> T parse(Class<T> clazz) throws Exception {
 		JAXBContext context = JAXBContext.newInstance(clazz);
 		Unmarshaller unmarshaller = context.createUnmarshaller();
-		T instance = (T) unmarshaller.unmarshal(Thread.currentThread().getContextClassLoader().getResource("old-assembly-workflow.xml"));
+		T instance = (T) unmarshaller.unmarshal(Thread.currentThread()
+				.getContextClassLoader()
+				.getResource("old-assembly-workflow.xml"));
 		return instance;
 	}
 }
